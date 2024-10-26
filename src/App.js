e0351df1e0e39c3ff0593462f6958039359fd81e
YakupKarakoc <krkcyakupp@gmail.com>
2024-10-26 18:43:52 +0300

    Ogrenciden Ogrenciye
    Bitirme Projesi Front-End Kısmı

---
+++ src/App.js
@@ -0,0 +1,21 @@
+import React from "react";
+import { BrowserRouter as Router, Routes, Route, Navigate } from "react-router-dom";
+import Signup from "./components/Signup";
+import Login from "./components/Login";
+import Home from "./components/Home";
+
+function App() {
+  return (
+    <Router>
+      <Routes>
+        {/* Varsayılan olarak giriş sayfasına yönlendirme */}
+        <Route path="/" element={<Navigate to="/login" replace />} />
+        <Route path="/signup" element={<Signup />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/home" element={<Home />} />
+      </Routes>
+    </Router>
+  );
+}
+
+export default App;
